a2726966bd8939ce8d614fa21c3d77f2ec4f858e
Aleix <aleix.fort45@gmail.com>
2023-04-06 16:22:56 +0200

    Added files

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
+from pymongo import MongoClient
+import requests
+
 
 app = Flask(__name__)
 
+
+test_api_endpoint = "https://api.npoint.io/786a14060decfb7e66d9"
+test_api_response = requests.get(test_api_endpoint)
+# check the response status code to ensure the request was successful
+if test_api_response.status_code == 200:
+    # if the request was successful, get the response data as a dictionary
+    data_dict = test_api_response.json()
+    # do something with the data dictionary
+    print(data_dict)
+else:
+    # if the request was unsuccessful, print an error message
+    print("Error: API request failed with status code", test_api_response.status_code)
+
+
 @app.route("/")
+def index():
+    return redirect(url_for("home"))
+
+@app.route("/home")
 def home():
+
+
     return render_template("home.html")
+
+@app.route("/stats")
+def stats():
+    return render_template("stats.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
